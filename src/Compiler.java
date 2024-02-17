dfc9c0c9d9c614bd6cb0622f36c9feaa3edbe199
Gianna Julio <46408848+g-juli0@users.noreply.github.com>
2024-02-17 12:06:54 -0500

    initialize repo

---
+++ src/Compiler.java
@@ -0,0 +1,11 @@
+/*
+ * main program for compiler
+ */
+
+
+public class Compiler {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Lexer lex = new Lexer();
+    }
+}
---
+++ src/Lexer.java
@@ -0,0 +1,12 @@
+/*
+ * Lexer class for compiler
+ */
+
+public class Lexer {
+    
+    public Lexer() {
+        System.out.println("lex");
+    }
+
+}
+

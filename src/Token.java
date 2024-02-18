1a04f76b3fd4b11daaf46d61e976cf3b508b94e2
Gianna Julio <46408848+g-juli0@users.noreply.github.com>
2024-02-18 14:11:21 -0500

    Token class buildout and documentation

--- src/Token.java
+++ src/Token.java
@@ -8,11 +8,50 @@ public class Token {
     private Kind kindOfToken;
     private String value;
 
+    /**
+     * Token constructor
+     * @param k Kind of Token
+     * @param v value of Token
+     */
     public Token(Kind k, String v) {
-        kindOfToken = k;
-        value = v;
+        this.kindOfToken = k;
+        this.value = v;
+    }
+
+    /**
+     * Token constructor
+     * @param k Kind of Token
+     */
+    public Token(Kind k) {
+        this.kindOfToken = k;
+        this.value = ""; // no value associated
+                         // with this kind of Token
+    }
+
+    /**
+     * getter for kind of Token
+     * @return Kind of Token
+     */
+    public Kind getKind() {
+        return this.kindOfToken;
+    }
+
+    /**
+     * getter for value of Token
+     * @return value of token
+     */
+    public String getValue() {
+        return this.value;
     }
     
+    /**
+     * @Override
+     * Token toString method
+     * @return Kind of Token converted to a String
+     */
+    public String toString() {
+        return this.kindOfToken.toString();
+    }
 }
 
 enum Kind {
